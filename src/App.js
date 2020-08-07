1d634ebb6cfbd66935c4f69ffda155aa030e797e
Erick Saavedra <esvdrah@gmail.com>
2020-08-07 18:10:02 -0500

    add Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './components/Header';
 
 function App() {
-  return <h1>News</h1>;
+  return (
+    <Fragment>
+      <Header title='News Search' />
+      <div className='container white'></div>
+    </Fragment>
+  );
 }
 
 export default App;
